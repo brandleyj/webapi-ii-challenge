b1dd5eedd18941966d93d8dff3adbcddeb054063
Justin Brandley <brandleyj@gmail.com>
2019-10-29 17:06:05 -0500

    added post and get requests

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -3,3 +3,28 @@ const router = require("express").Router();
 const comments = require("../data/seeds/02-comments");
 
 module.exports = router;
+
+router.post("/:id/comments", (req, res) => {
+	const comment = req.body;
+
+	if (!comment.postID) {
+		res
+			.status(404)
+			.json({ message: "The post with the specified ID does not exist." });
+	} else if (!comment.text) {
+		res
+			.status(400)
+			.json({ errorMessage: "Please provide text for the comment." });
+	} else if (comment.postID && comment.text) {
+		comments
+			.insertComment(comment)
+			.then(result => {
+				res.status(201).json(result);
+			})
+			.catch(err => {
+				res.status(500).json({
+					err: "Could not insert comment. Check post structure & try again."
+				});
+			});
+	}
+});
--- routes/posts-router.js
+++ routes/posts-router.js
@@ -3,3 +3,54 @@ const router = require("express").Router();
 const posts = require("../data/seeds/01-posts");
 
 module.exports = router;
+
+router.post("/", (req, res) => {
+	const postInfo = req.body;
+	if (!postInfo.title || !postInfo.contents) {
+		res.status(400).json({
+			errorMessage: "Please provide title and contents for the post."
+		});
+	} else {
+		posts
+			.insert(postData)
+			.then(post => {
+				res.status(201).json(post);
+			})
+			.catch(error => {
+				res.status(500).json({
+					error: "There was an error while saving the post to the database"
+				});
+			});
+	}
+});
+
+router.get("/", (req, res) => {
+	posts
+		.find()
+		.then(post => res.json(post))
+		.catch(error =>
+			res.status(500).json({
+				error: "The posts information could not be retrieved."
+			})
+		);
+});
+
+router.get("/:id", (req, res) => {
+	const id = req.params.id;
+	posts
+		.findById(id)
+		.then(postsById => {
+			if (postsById) {
+				res.json(postsById);
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist."
+				});
+			}
+		})
+		.catch(error =>
+			res
+				.status(500)
+				.json({ error: "The post information could not be retrieved." })
+		);
+});
